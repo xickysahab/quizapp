172c62eeb7e11dae402df7257014843cde3b6a11
Aagam <jainaagam908@gmail.com>
2026-08-01 20:41:16 +0530

    Automate database sync on build and update Prisma adapter

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,9 +1,11 @@
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://aagamjain@localhost:5432/quiz?schema=public';
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
